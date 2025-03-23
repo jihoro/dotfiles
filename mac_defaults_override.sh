ba180eb4e2d6cca0ee467da17d5fea32fbe17b4c
Jiho Ro <jihoandy@gmail.com>
2025-03-23 00:47:17 -0500

    make key repeats faster on mac

---
+++ mac_defaults_override.sh
@@ -0,0 +1,4 @@
+defaults write -g ApplePressAndHoldEnabled -bool false
+defaults write -g NSWindowShouldDragOnGesture -bool true
+defaults write -g InitialKeyRepeat -int 10
+defaults write -g KeyRepeat -int 1
